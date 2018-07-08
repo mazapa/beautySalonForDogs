94fbf22f2aa6f1c59ac6d1aef6c2fa3972600112
= <=>
2018-07-08 23:12:30 +0300

    add change sign out command

---
+++ src/main/java/by/ryazantseva/salon/logic/SendEmailPasswordLogic.java
@@ -0,0 +1,30 @@
+package by.ryazantseva.salon.logic;
+
+import by.ryazantseva.salon.dao.UserDAO;
+import by.ryazantseva.salon.entity.User;
+import by.ryazantseva.salon.validation.EncryptPassword;
+import by.ryazantseva.salon.validation.InputUserDataValidation;
+
+public class SendEmailPasswordLogic {
+    private User user;
+
+    public boolean changePassword(User user, String oldPassword, String newPassword, String repeatNewPassword) {
+        oldPassword = EncryptPassword.sha1(oldPassword);
+        if (user.getPassword().equals(oldPassword)) {
+            if (InputUserDataValidation.checkPassword(newPassword) && InputUserDataValidation.checkPasswordEquals(newPassword, repeatNewPassword)) {
+                user.setPassword(newPassword);
+                UserDAO dao = new UserDAO();
+                dao.update(user);
+                this.user = user;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public User getUser() {
+        return user;
+
+    }
+
+}
